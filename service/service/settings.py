4b6ddde429a9df6d2542c31b379d8a9f68fe8a6e
kycool <kycoolcool@gmail.com>
2018-10-18 11:06:03 +0800

    添加 ETAG 支持

--- service/service/settings.py
+++ service/service/settings.py
@@ -68,10 +68,11 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
 ]
 
 ROOT_URLCONF = 'service.urls'
