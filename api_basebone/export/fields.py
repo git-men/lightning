0a352fa3cb3fa7dd60cbc366a5dce14aca4aaf0f
kycool <kycoolcool@gmail.com>
2018-11-16 11:25:49 +0800

    调整输出 schema 配置

--- api_basebone/export/fields.py
+++ api_basebone/export/fields.py
@@ -32,26 +32,52 @@ DJANGO_FIELD_TYPE_MAP = {
     'BoneImageUrlField': 'Image',
 }
 
+GMETA_TITLE_FIELD = 'title_field'
+GMETA_FIELD_CONFIG = 'field_form_config'
+
+# django 中声明的字段的配置和输出配置键的映射
+GMETA_FIELD_CONFIG_MAP = {
+    'verbose_name': 'displayName'
+}
+
 
 def get_gmeta_class(model):
     """获取模型中的 GMeta 类"""
     return getattr(model, 'GMeta', None)
 
 
+def get_attr_in_gmeta_class(model, config_name, default_value=None):
+    """获取指定模型 GMeta 类中指定的属性
+
+    Params:
+        model class django 模型类
+        config_name string GMeta 类中配置项的名称
+        default_value 任何数据类型 默认数据
+    """
+
+    gmeta_class = get_gmeta_class(model)
+    if not gmeta_class:
+        return default_value
+    return getattr(gmeta_class, config_name, default_value)
+
+
 class FieldConfig:
 
-    def _get_extra_params(self, field, data_type):
-        """获取 Gmeta 中声明的表单的重置的配置"""
-        gmeta_class = get_gmeta_class(field.model)
-        if not gmeta_class:
-            return {}
-        field_form_config = getattr(gmeta_class, 'field_form_config', None)
-        if not field_form_config:
-            return {}
+    def reset_field_config(self, field, data_type=None):
+        """根据 Gmeta 中声明的字段的配置进行重置"""
+        field_config = get_attr_in_gmeta_class(field.model, GMETA_FIELD_CONFIG, {}).get(field.name, {})
+        if not field_config:
+            return field_config
 
-        return field_form_config.get(field.name, {})
+        # 做 django 中的写法和输出的配置的转换
+        result = {
+            GMETA_FIELD_CONFIG_MAP[key] if key in GMETA_FIELD_CONFIG_MAP else key: value
+            for key, value in field_config.items()
+        }
+        return result
 
     def _get_common_field_params(self, field, data_type):
+        """获取字段的通用的配置"""
         config = {
             'name': field.name,
             'displayName': field.verbose_name,
@@ -73,39 +99,48 @@ class FieldConfig:
         return config
 
     def normal_field_params(self, field, data_type):
+        """通用普通字段的配置获取"""
         base = self._get_common_field_params(field, data_type)
-        base.update(self._get_extra_params(field, data_type))
+        base.update(self.reset_field_config(field, data_type))
         return base
 
     def string_params(self, field, data_type):
+        """字符型字段的配置获取"""
         base = self._get_common_field_params(field, data_type)
         base['maxLength'] = field.max_length
-        base.update(self._get_extra_params(field, data_type))
+        base.update(self.reset_field_config(field, data_type))
         return base
 
     def ref_params(self, field, data_type):
+        """外键关联字段的配置获取"""
         base = self._get_common_field_params(field, data_type)
         meta = field.related_model._meta
         base['ref'] = '{}__{}'.format(meta.app_label, meta.model_name)
+        base.update(self.reset_field_config(field, data_type))
         return base
 
     def refmult_params(self, field, data_type):
+        """多对多字段的配置获取"""
         base = self._get_common_field_params(field, data_type)
         meta = field.related_model._meta
         base['ref'] = '{}__{}'.format(meta.app_label, meta.model_name)
-        base.update(self._get_extra_params(field, data_type))
+        base.update(self.reset_field_config(field, data_type))
         return base
 
 
-field_config = FieldConfig()
+field_config_instance = FieldConfig()
 
 
 def get_model_field_config(model):
+    """获取指定模型的字段配置, 字段输出所有的字段，包含反向字段
+
+    Params:
+        model class django 模型类
+    """
     fields = get_concrete_fields(model)
     key = '{}__{}'.format(model._meta.app_label, model._meta.model_name)
 
-    model_gmeta = get_gmeta_class(model)
-    title_field = 'id' if model_gmeta is None else getattr(model_gmeta, 'title_field', 'id')
+    title_field = get_attr_in_gmeta_class(model, GMETA_TITLE_FIELD, 'id')
 
     config = []
     for item in fields:
@@ -113,7 +148,6 @@ def get_model_field_config(model):
 
         # 抓取 internal_type
         field_type_func = getattr(item, 'get_bsm_internal_type', None)
-
         if field_type_func:
             field_type = DJANGO_FIELD_TYPE_MAP.get(item.get_bsm_internal_type())
         else:
@@ -121,11 +155,11 @@ def get_model_field_config(model):
 
         if field_type is not None:
             data_type = FIELDS.get(field_type)['name']
-            function = getattr(field_config, '{}_params'.format(field_type.lower()), None)
+            function = getattr(field_config_instance, '{}_params'.format(field_type.lower()), None)
             if function is not None:
                 config.append(function(item, data_type))
             else:
-                config.append(field_config.normal_field_params(item, data_type))
+                config.append(field_config_instance.normal_field_params(item, data_type))
 
     # 添加反转字段
     reverse_fields = get_reverse_fields(model)
@@ -142,8 +176,12 @@ def get_model_field_config(model):
             }
             meta = item.related_model._meta
             model_verbose_name = meta.verbose_name if meta.verbose_name else meta.model_name
+
             reverse_config['displayName'] = model_verbose_name
             reverse_config['ref'] = '{}__{}'.format(meta.app_label, meta.model_name)
+            reverse_config.update(
+                field_config_instance.reset_field_config(item)
+            )
             config.append(reverse_config)
 
     return {
