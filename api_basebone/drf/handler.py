3e2a32009c3511fdaa02c4c95569c33c7f149876
kycool <kycoolcool@gmail.com>
2018-11-29 17:06:18 +0800

    - 添加可配置严重错误的直接渲染的开关

--- api_basebone/drf/handler.py
+++ api_basebone/drf/handler.py
@@ -38,7 +38,11 @@ def exception_handler(exc, context):
     if response:
         return response
 
-    if not settings.DEBUG:
+    # 可自由配置是否直接抛出严重的错误
+    CLOSE_DIRECT_SERIOUS_ERROR_SHOW = getattr(
+        settings, 'CLOSE_DIRECT_SERIOUS_ERROR_SHOW', True)
+
+    if CLOSE_DIRECT_SERIOUS_ERROR_SHOW:
         # 记录错误日志到对应的日志处理器中
         logger = logging.getLogger('django')
         logger.exception(exc)
