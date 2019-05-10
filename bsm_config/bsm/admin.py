1d35e36bd37b10b7e12aa3e370ee1998c3d07b96
jeff <bbmyth@gmail.com>
2019-05-10 18:51:31 +0800

    使用组合字段显示菜单名

--- bsm_config/bsm/admin.py
+++ bsm_config/bsm/admin.py
@@ -5,10 +5,19 @@ from bsm_config.models import Menu
 
 @register
 class MenuAdmin(BSMAdmin):
-    display = ['icon', 'name', 'page', 'permission', 'model']
+    display = [
+        {
+            'type': 'combo',
+            'name': 'displayName',
+            'displayName': '名称',
+            'template': 'iconText',
+            'mapping': {'icon': 'icon', 'text': 'name'}
+        },
+        'page', 'permission', 'model', 'sequence']
     form_fields = [
-        'name', 'icon', 'parent', 'page', 'permission', 'model'
+        'name', 'icon', 'parent', 'page', 'permission', 'model', 'sequence'
     ]
+    inlineFormFields = ['sequence']
     display_in_tree = True  # 树型列表
 
     class Meta:
