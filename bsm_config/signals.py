917af24d167fa1bd53d9dbd01786121ee47ee6ef
laoxiangru <laoxiangru.com>
2020-06-03 16:28:35 +0800

    修复菜单权限错乱bug

--- bsm_config/signals.py
+++ bsm_config/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import Permission, Group
 
 from .models import Menu
-from .utils import create_permission, remove_permission, check_page
+from .utils import remove_permission, check_page, get_permission
 
 @receiver(pre_save, sender=Menu, dispatch_uid='remove_menu_permission')
 def remove_menu_permission(sender, instance, update_fields = [], **kwargs):
@@ -11,8 +11,10 @@ def remove_menu_permission(sender, instance, update_fields = [], **kwargs):
         old_instance = Menu.objects.get(id=instance.id)
         old_page = old_instance.page
         new_page = instance.page
+        old_type = old_instance.type
+        new_type = instance.type
 
-        if check_page(old_page, new_page):
+        if check_page(old_page, new_page, old_type, new_type):
             remove_permission(old_instance)
 
         if new_page not in (Menu.PAGE_LIST, Menu.PAGE_DETAIL):
@@ -21,10 +23,27 @@ def remove_menu_permission(sender, instance, update_fields = [], **kwargs):
 
 @receiver(post_save, sender=Menu, dispatch_uid='set_menu_permission')
 def set_menu_permission(sender, instance, update_fields = [], **kwargs):
-    permission = create_permission(instance)
-    Menu.objects.filter(id=instance.id).update(permission=permission)
+    _, permission_lable = get_permission(instance)
+    Menu.objects.filter(id=instance.id).update(permission=permission_lable)
 
 @receiver(pre_delete, sender=Menu, dispatch_uid='delete_menu_permission')
 def delete_menu_permission(sender, instance, **kwargs):
+    permission, _ = get_permission(instance)
+    permission.group_set.remove(*instance.groups.all().exclude(name='系统管理员'))
     remove_permission(instance)
-        
+
+from django.db.models.signals import m2m_changed
+from django.contrib.auth.models import Permission, Group
+
+
+def menu_changed(instance, model, pk_set, action, **kwargs):
+    # raise Exception('jfsdfjsdaa')
+    if action in ('post_add', 'post_remove'):
+        # menu = Menu.objects.get(sender.menu_id)
+        groups = model.objects.filter(pk__in=list(pk_set))
+        permission, _ = get_permission(instance)
+        if action == 'post_add':
+            permission.group_set.add(*groups)
+        if action == 'post_remove':
+            permission.group_set.remove(*groups)
+m2m_changed.connect(menu_changed, sender=Menu.groups.through)
--- bsm_config/utils.py
+++ bsm_config/utils.py
@@ -7,41 +7,20 @@ from .models import Menu, Setting
 MODEL_PAGES = (Menu.PAGE_LIST, Menu.PAGE_DETAIL)
 ORDER_PAGES = (Menu.PAGE_ADMIN_CONFIG, Menu.PAGE_AUTO, Menu.PAGE_CHART, Menu.PAGE_IFRAME)
 
-def create_permission(menu):
-    if menu.page in MODEL_PAGES:
-        app, model = menu.model.split('__')
-        permission = Permission.objects.filter(content_type__app_label=app,content_type__model=model,codename=f'view_{model}').first()
-     
-    if menu.page in ORDER_PAGES or menu.type == Menu.TYPE_GROUP:
-        codename = f'menu_view_{menu.page or menu.type}_{menu.id}'   
-        content_type = ContentType.objects.get_for_model(Menu)
-        permission, _ = Permission.objects.get_or_create(codename=codename, name=codename,content_type=content_type)
-    
-    permission.group_set.set(menu.groups.all())
-    return f'{permission.content_type.app_label}.{permission.codename}'
-
 def remove_permission(menu):
-    if not menu.permission:
-        return
-    _ , codename = menu.permission.split('.')
-
-    if menu.page in MODEL_PAGES:
-        app, model = menu.model.split('__')
-        isinstance = apps.get_app_config(app).get_model(model) 
-        content_type = ContentType.objects.get_for_model(isinstance)
-    else:    
-        content_type = ContentType.objects.get_for_model(Menu)
-
-    permission = Permission.objects.get(codename=codename, content_type=content_type)
-    permission.group_set.clear()
-
-    if menu.page in ORDER_PAGES:
+    # 删除菜单权限
+    permission, _ = get_permission(menu)
+    if menu.page in ORDER_PAGES or menu.type == Menu.TYPE_GROUP:
         permission.delete()
 
 
-def check_page(old_page, new_page):
+
+# 检查页面、菜单类型是否更改
+def check_page(old_page, new_page, old_type, new_type):
     page1 = MODEL_PAGES
     page2 = ORDER_PAGES
+    if old_type == new_type == Menu.TYPE_GROUP:
+        return False
 
     if (old_page in page1 and new_page in page1) or (old_page in page2 and new_page in page2):
         return False
@@ -66,6 +45,37 @@ def create_menus_permission(menus):
 
     Menu.objects.bulk_update(menus, ['permission'])
     Permission.objects.bulk_create(permissions)
+
+def get_permission(menu):
+    if menu.page in MODEL_PAGES:
+        app, model = menu.model.split('__')
+        
+    content_model = Menu
+
+    if not menu.permission:
+        # 创建菜单权限
+        if menu.page in MODEL_PAGES:
+            content_model = apps.get_app_config(app).get_model(model) 
+            content_type = ContentType.objects.get_for_model(content_model) 
+            permission = Permission.objects.get(codename=f'view_{model}', content_type=content_type)
+     
+        if menu.page in ORDER_PAGES or menu.type == Menu.TYPE_GROUP:
+            codename = f'menu_view_{menu.page or menu.type}_{menu.id}'   
+            content_type = ContentType.objects.get_for_model(content_model)
+            permission, _ = Permission.objects.get_or_create(codename=codename, name=codename, content_type=content_type)
+
+    else:
+        # 通过menu.permission字符串获取权限实例
+        _ , codename = menu.permission.split('.')
+        if menu.page in MODEL_PAGES:
+            content_model = apps.get_app_config(app).get_model(model) 
+        content_type = ContentType.objects.get_for_model(content_model)        
+        permission = Permission.objects.get(codename=codename, content_type=content_type)
+    
+    return permission,  f'{permission.content_type.app_label}.{permission.codename}'
+
+
+
 
 
 
