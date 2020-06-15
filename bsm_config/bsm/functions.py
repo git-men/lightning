74080b352706cf5f1b78fb40030c4a602d694fb9
laoxiangru <laoxiangru.com>
2020-06-15 10:44:37 +0800

    添加配置权限云函数

--- bsm_config/bsm/functions.py
+++ bsm_config/bsm/functions.py
@@ -1,9 +1,14 @@
 from django.db import transaction
-from django.contrib.auth.models import User
+from django.contrib.auth.models import Permission
+from django.contrib.auth import get_user_model
+
+from guardian.shortcuts import get_objects_for_user
 
 from api_basebone.restful.funcs import bsm_func
 from bsm_config.models import Setting
-from api_basebone.drf.response import success_response, error_response
+from api_basebone.restful.serializers import multiple_create_serializer_class
+
+User = get_user_model()
 
 def init_setting():
     settings = [
@@ -39,3 +44,13 @@ def update_user(user=None, **kwargs):
         user.save()
     else:
         raise RuntimeError('old password error')
+    
+
+@bsm_func(staff_required=True, name='get_assign_permissions', model=Permission)
+def get_assign_permissions(user, **kwargs):
+    permissions = get_objects_for_user(user, 'auth.permission_assign').select_related('content_type')
+    serializer_class = multiple_create_serializer_class( 
+        Permission, 
+        expand_fields=['content_type'], 
+    ) 
+    return [serializer_class(per).data for per in permissions ]
