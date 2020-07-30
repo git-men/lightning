d3103a32cb535062c8d8d58513af386f197d2868
laoxiangru <laoxiangru.com>
2020-07-30 18:58:29 +0800

    Author kycool/修复不是有USE_TZ=False列表时间查询的bug

--- api_basebone/db/__init__.py
+++ api_basebone/db/__init__.py
@@ -1,9 +1,24 @@
 import inspect
 import types
+from functools import wraps
 
 from django.apps import apps
+from django.db.models.fields import DateTimeField
 
 from .query import QuerySetExtend
+from .fields import DateTimeFieldExtend
+
+
+def add_method(cls):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            return func(*args, **kwargs)
+
+        setattr(cls, func.__name__, wrapper)
+        return func
+
+    return decorator
 
 
 def extend_objects(queryset_class):
@@ -16,4 +31,11 @@ def extend_objects(queryset_class):
             setattr(model.objects, name, types.MethodType(method, model.objects))
 
 
+def extend_method(origin_class, extend_class):
+
+    for name, method in inspect.getmembers(extend_class, predicate=inspect.isfunction):
+        setattr(origin_class, name, method)
+
+
 extend_objects(QuerySetExtend)
+extend_method(DateTimeField, DateTimeFieldExtend)
