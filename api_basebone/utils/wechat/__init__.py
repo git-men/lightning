062c26b1f9055d03fc9513e7d31f6d27409bf93c
laoxiangru <laoxiangru.com>
2020-09-20 23:33:23 +0800

    utils支持返回wechatpy client

--- api_basebone/utils/wechat/__init__.py
+++ api_basebone/utils/wechat/__init__.py
@@ -11,11 +11,20 @@ session = RedisStorage(get_redis_connection())\
     if settings.CACHES['default']['BACKEND'] == 'django_redis.cache.RedisCache'\
         else MemoryStorage()
 
+
 def wrap(api, app_id, app_secret=None):
-    return api(
-        client=WeChatClient(appid=app_id, secret=app_secret or settings.WECHAT_APP_MAP[app_id]['appsecret'], session=session)
-    )
+    return api(client=WeChatClient(
+        appid=app_id,
+        secret=app_secret or settings.WECHAT_APP_MAP[app_id]['appsecret'],
+        session=session))
 
 
 def wxa(app_id, app_secret=None):
     return wrap(WeChatWxa, app_id=app_id, app_secret=app_secret)
+
+
+def wechat_client(app_id, app_secret=None):
+    return WeChatClient(app_id,
+                        secret=app_secret
+                        or settings.WECHAT_APP_MAP[app_id]['appsecret'],
+                        session=session)
