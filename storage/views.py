06c39de70fe08f8c5a8b6d495e54a851e963a493
Ivan <qwIvan@users.noreply.github.com>
2020-11-04 19:38:27 +0800

    支持文件系统存储上传的文件

---
+++ storage/views.py
@@ -0,0 +1,36 @@
+import os
+from pathlib import Path
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import MultiPartParser
+
+from api_basebone.core.exceptions import BusinessException
+from api_basebone.drf.response import success_response
+from bsm_config.settings import site_setting
+
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser])
+def upload(request):
+    key, policy, file = request.data['key'], request.data['policy'], request.data['file']
+    storage_path = site_setting['storage_path']
+    if not storage_path:
+        raise BusinessException('storage support not enabled')
+    file_path = Path(storage_path).joinpath(key)
+    if not os.path.abspath(file_path).startswith(storage_path):
+        # 避免 path traversal 问题 https://owasp.org/www-community/attacks/Path_Traversal
+        # Python 3.9 才有 Path.is_relative_to，
+        # 而 Path.resolve 会 follow symbol link，
+        # Path.absolute 又不能解析“../”，
+        # 所以只能用 os.path.abspath 了
+        raise BusinessException('invalid file key: %s' % key)
+    dirname = file_path.parent
+    if not dirname.exists():
+        dirname.mkdir(parents=True)
+    elif not dirname.is_dir():
+        raise BusinessException('dir exists: %s' % os.path.dirname(key))
+    elif file_path.exists():
+        raise BusinessException('file already exists: %s' % key)
+    with file_path.open('wb+') as f:
+        for chunk in file.chunks():
+            f.write(chunk)
+    return success_response()
