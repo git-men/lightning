9dbc788f8d2449bb1575fcb8c1ca315631c3ced0
haoxiansheng <haoxiansheng.com>
2020-11-26 17:05:55 +0800

    配置菜单admin

--- bsm_config/bsm/admin.py
+++ bsm_config/bsm/admin.py
@@ -25,7 +25,7 @@ class MenuAdmin(BSMAdmin):
         {'name': 'parent', 'widget': 'Cascader'},
         {'name': 'type', 'widget': 'Radio'},
         {'name': 'page', 'show': '${type} === "item"'},
-        {'name': 'puzzle', 'show': '${page} === "chart"'},
+        {'name': 'puzzle', 'show': '${page} === "puzzle"'},
         {'name': 'model', 'widget': 'ModelSelect', 'show': '(${page} === "list" || ${page} === "detail") && ${type} === "item" || ${page} === "map"'},
         {'name': 'path', 'show': '${page} === "auto" || ${page} === "iframe"'},
         'groups'
