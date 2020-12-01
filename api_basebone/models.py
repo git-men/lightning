61313bea395e7346232d5e8245cf787196ec2e8e
jeff <bbmyth@gmail.com>
2020-12-01 21:35:54 +0800

    使用触发器生成操作日志

--- api_basebone/models.py
+++ api_basebone/models.py
@@ -1,8 +1,12 @@
 import logging
 
+from django.dispatch import receiver
 from django.conf import settings
 from django.db import models
+
 from api_basebone.core.fields import JSONField
+from api_basebone.signals import post_bsm_create, post_bsm_delete
+from api_basebone.settings import settings as basebone_settings
 
 logger = logging.getLogger('django')
 
@@ -22,3 +26,47 @@ class AdminLog(models.Model):
     class Meta:
         verbose_name = '动作日志记录'
         verbose_name_plural = '动作日志记录'
+
+
+@receiver(post_bsm_create, dispatch_uid='__append_create_log')
+def append_create_log(sender, instance, create, request, old_instance, scope, **kwargs):
+    if sender == AdminLog or not basebone_settings.MANAGE_USE_ACTION_LOG or scope != 'admin':
+        return
+    try:
+        action = 'add' if create else 'update'
+        gmeta = getattr(sender, 'GMeta', None)
+        title_field = getattr(gmeta, 'title_field', None) if gmeta else None
+
+        AdminLog.objects.create(
+            user=request.user,
+            action=action,
+            app_label=sender._meta.app_label,
+            model_slug=sender._meta.model_name,
+            object_id=instance.pk,
+            params=request.data,
+            message=getattr(instance, title_field) if title_field else repr(instance)
+        )
+    except:
+        logger.error('append create log fail', exc_info=True)
+    
+
+@receiver(post_bsm_delete, dispatch_uid='__append_delete_log')
+def append_delete_log(sender, instance, request, scope, **kwargs):
+    print('增加删除的Log')
+    if sender == AdminLog or not basebone_settings.MANAGE_USE_ACTION_LOG or scope != 'admin':
+        return
+    try:
+        action = 'delete'
+        gmeta = getattr(sender, 'GMeta', None)
+        title_field = getattr(gmeta, 'title_field', None) if gmeta else None
+
+        AdminLog.objects.create(
+            user=request.user,
+            action=action,
+            app_label=sender._meta.app_label,
+            model_slug=sender._meta.model_name,
+            object_id=instance.pk,
+            message=getattr(instance, title_field) if title_field else repr(instance)
+        )
+    except:
+        logger.error('append delete log fail', exc_info=True)
