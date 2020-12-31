8ce8997af4dda314e924431165a0bfb423a82007
jeff <bbmyth@gmail.com>
2020-12-31 10:41:17 +0800

    云函数及信号自动发现

--- lightning/apps.py
+++ lightning/apps.py
@@ -1,6 +1,7 @@
 import sys
 from django.contrib.auth import get_user_model
 from django.apps import AppConfig
+from django.conf import settings
 from api_basebone.utils import module
 
 
@@ -12,3 +13,12 @@ class LightningConfig(AppConfig):
         path = user_model._meta.app_config.name + '.bsm.' + module.BSM_FORM
         from .bsm import user_forms
         sys.modules[path] = user_forms
+
+        apps = settings.INSTALLED_APPS
+        for app in apps:
+            try:
+                __import__('.'.join([app, 'functions']))
+                __import__('.'.join([app, 'signals']))
+            except:
+                pass
+        
