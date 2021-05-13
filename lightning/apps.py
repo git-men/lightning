73519aac69402a0bc79796db500d0ede7cadf9eb
jeff <bbmyth@gmail.com>
2021-05-13 09:57:15 +0800

    fix functions auto load issue

--- lightning/apps.py
+++ lightning/apps.py
@@ -1,9 +1,12 @@
 import sys
 import os
+import logging
 from django.contrib.auth import get_user_model
 from django.apps import AppConfig
 from django.conf import settings
 from api_basebone.utils import module
+
+log = logging.getLogger('lightning')
 
 
 class LightningConfig(AppConfig):
@@ -20,12 +23,13 @@ class LightningConfig(AppConfig):
             try:
                 # TODO INSTALLED_APPS 里配的内容不一定是module，还不一定是字符串。提前导入也会改变导入顺序，使一些云函数的覆盖逻辑出错。
                 app_module = __import__(app)
-                functions_path = os.path.join(path, 'functions')
                 path = app_module.__path__[0]
+                functions_path = os.path.join(path, 'functions')
                 signal_path = os.path.join(path, 'signals')
                 if os.path.exists(functions_path + '.py'):
                     __import__('.'.join([app, 'functions']))
                 if os.path.exists(signal_path + '.py'):
                     __import__('.'.join([app, 'signals']))
             except:
+                log.error(f'自动发现脚本发生异常: {functions_path}', exc_info=True)
                 pass
