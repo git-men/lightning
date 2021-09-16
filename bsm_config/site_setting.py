1c82cc1c942f23281fbd268296d84f947423d19d
Ivan <7613160+qwIvan@users.noreply.github.com>
2021-09-16 19:29:41 +0800

    site_settings可以在class中覆盖某些行为

--- bsm_config/site_setting.py
+++ bsm_config/site_setting.py
@@ -1,8 +1,26 @@
 from django.conf import settings
 
 
+def default_get_field(field):
+    f = {
+        "displayName": field.get('displayName',''),
+        "help": field.get('help',''),
+        "name": field['name'],
+        "type": field.get('type','string'),
+        "required": field.get('required',False),
+    }
+    if 'choices' in field:
+        f['choices'] = field['choices']
+    if 'default' in field:
+        f['default'] = field['default']
+    if 'validators' in field:
+        f['validators'] = field['validators']
+    return f
+
+
 class Field:
     type = None
+    get_field = staticmethod(default_get_field)
 
     def __init__(self, **kwargs):
         self.inner_dict = kwargs
@@ -27,11 +45,44 @@ class DecimalField(Field):
     type = 'decimal'
 
 
+class ArrayField(Field):
+    type = 'array'
+
+    def get_field(self, field_dict):
+        result = super().get_field(field_dict)
+        # TODO
+        return result
+
+
+class FieldDict(dict):
+    def __init__(self, name, field_type):
+        super().__init__(name=name, **field_type.to_dict())
+        self.field_type = field_type
+
+    def get_field(self):
+        return self.field_type.get_field(self)
+
+
+def default_get_schemas(default_schemas):
+    return default_schemas
+
+
 class Panel:
+    get_schemas = staticmethod(default_get_schemas)
+
     @classmethod
     def to_dict(cls, meta):
-        meta['fields'] = [{'name': k, **v.to_dict()} for k, v in cls.__dict__.items() if isinstance(v, Field)]
+        meta['fields'] = [FieldDict(k, v) for k, v in cls.__dict__.items() if isinstance(v, Field)]
         return meta
+
+
+class PanelDict(dict):
+    def __init__(self, panel, **kwargs):
+        super().__init__(**panel.to_dict(kwargs))
+        self.panel = panel
+
+    def get_schemas(self, default_schemas):
+        return self.panel.get_schemas(default_schemas)
 
 
 class PanelMeta:
@@ -41,7 +92,7 @@ class PanelMeta:
         self.kwargs = kwargs
 
     def __call__(self, panel: Panel):
-        settings.WEBSITE_CONFIG.append(panel.to_dict(self.kwargs))
+        settings.WEBSITE_CONFIG.append(PanelDict(panel, **self.kwargs))
 
 
 def register_panel(**kwargs):
